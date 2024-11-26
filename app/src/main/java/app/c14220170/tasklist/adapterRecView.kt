281b5949ed4715fbf92f02d8a2deebc50d16dff7
C14220170 <C14220170@john.petra.ac.id>
2024-11-26 22:10:02 +0700

    Initial commit

---
+++ app/src/main/java/app/c14220170/tasklist/adapterRecView.kt
@@ -0,0 +1,7 @@
+package app.c14220170.tasklist
+
+import androidx.recyclerview.widget.RecyclerView
+
+class adapterRecView(private val listTask: <ArrayList<task>) : RecyclerView.Adapter<adapterRecView.ListViewHolder>() {
+
+}
---
+++ app/src/main/java/app/c14220170/tasklist/task.kt
@@ -0,0 +1,8 @@
+package app.c14220170.tasklist
+
+data class task(
+    var title: String,
+    var desc: String,
+    var startDate: String,
+    var endDate: String
+)
